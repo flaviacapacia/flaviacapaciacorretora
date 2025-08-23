e97283dee031231a8bfa079510a5a734d0c8eb44
flaviacapacia <154066227+flaviacapacia@users.noreply.github.com>
2025-08-23 17:01:32 -0300

    Update script.js
    Signed-off-by: flaviacapacia <154066227+flaviacapacia@users.noreply.github.com>

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ form.addEventListener("submit", function (e) {
 
   const dados = new FormData(form);
 
- fetch("https://script.google.com/macros/s/AKfycbwkuOcAaaqfaIfRNE8dpKcPjvf-oAlKrR2UD_5QJPSo4LBUe0Tths3fb9lNrfUxdSyINw/exec", {
+ fetch(https://script.google.com/macros/s/AKfycbxOjsNWnKH_9M_OhH33pW1F3tlZeHOgEwlLc7X7nbMjYj4dd5sHow6reHNAp8pay4AtVQ/exec", {
   method: "POST",
   body: dados,
 })
@@ -50,6 +50,7 @@ fetch('https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/teci
     });
   })
   .catch(error => console.error("Erro ao carregar imóveis:", error));
+
 
 
 
