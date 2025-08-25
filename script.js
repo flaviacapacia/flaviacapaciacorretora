0197a8bd47e4044c24575d354e9490b7453d8b73
flaviacapacia <154066227+flaviacapacia@users.noreply.github.com>
2025-08-25 19:37:12 -0300

    Update script.js
    Signed-off-by: flaviacapacia <154066227+flaviacapacia@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
 
 /* Negocie */
 
+
 document.getElementById("formNegocie").addEventListener("submit", async function(e) {
   e.preventDefault();
 
-  const form = e.target;
-  const formData = new FormData(form);
+  const formData = new FormData(this);
   const mensagem = document.getElementById("mensagem");
-
   mensagem.textContent = "Enviando...";
-
+    
   try {
     const resposta = await fetch("https://script.google.com/macros/s/AKfycbyNqEQou--9KqorHzIZRxmivZvmY3XBmX7wy2IpJyswdW7Nd8mEc7mEQe9KNF45mhwKrg/exec", {
       method: "POST",
       body: formData
     });
-
-    const texto = await resposta.text();
+    const texto = await resp.text();
     mensagem.textContent = texto;
-    mensagem.style.color = resposta.ok ? "green" : "red";
-
-    if (resposta.ok) {
-      form.reset();
-    }
-  } catch (erro) {
-    mensagem.textContent = "Erro ao enviar: " + erro;
+    mensagem.style.color = resp.ok ? "green" : "red";
+    if (resp.ok) this.reset();
+  } catch (err) {
+    mensagem.textContent = "Erro: " + err;
     mensagem.style.color = "red";
   }
 });
-
-
-
-
-
-
 
 fetch('https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/integrations/5b33fb35-31ad-48a1-9ad7-78e3918ca78f/casa-mineira.xml')
   .then(response => response.text())
@@ -60,6 +49,7 @@ fetch('https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/teci
     });
   })
   .catch(error => console.error("Erro ao carregar imóveis:", error));
+
 
 
 
