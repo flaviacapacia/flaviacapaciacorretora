75d630bc00d7b4b60375b4bb23c31b6da532227c
flaviacapacia <154066227+flaviacapacia@users.noreply.github.com>
2025-08-26 18:31:10 -0300

    Update script.js
    Signed-off-by: flaviacapacia <154066227+flaviacapacia@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 /* Negocie */
 // URL do seu Google Apps Script publicado como Web App
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzzJ7wt-0JDIPlYtLWUt_NKJPfGQII_6TlqbxE0wcBy2ylSXfUmbGnXyMAoRK4Fjhl9/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxUmtr3qCNKhudgCzfYdScdA48iZZWIELZ2kpxFDMrMuit9lUzdK3NWiw169wUCSf0A/exec";
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("formNegocie");
@@ -37,31 +37,11 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-/*tecimob-production/o/integrations/5b33fb35-31ad-48a1-9ad7-78e3918ca78f/casa-mineira.xml')
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => {
-    const imoveis = data.querySelectorAll("imovel");
-    const container = document.getElementById("lista-imoveis-tecimob");
 
-    imoveis.forEach(imovel => {
-      const titulo = imovel.querySelector("titulo")?.textContent || "Sem título";
-      const descricao = imovel.querySelector("descricao")?.textContent || "";
-      const preco = imovel.querySelector("valor")?.textContent || "Sob consulta";
-      const imagem = imovel.querySelector("fotos foto")?.textContent || "img/imovel-default.jpg";
-
-      const card = document.createElement("div");
-      card.className = "imovel-card";
-      card.innerHTML = `
-        <img src="${imagem}" alt="${titulo}">
-        <h3>${titulo}</h3>
-        <p>${descricao}</p>
-        <strong>Valor: R$ ${preco}</strong>
-      `;
-      container.appendChild(card);
     });
   })
   .catch(error => console.error("Erro ao carregar imóveis:", error));
+
 
 
 
