462bd2d5241de566ce1032bddea0767f06d368d8
flaviacapacia <154066227+flaviacapacia@users.noreply.github.com>
2025-10-27 14:43:24 -0300

    Add comments for clarity in Program.cs
    Added comments to clarify sections of the code.
    
    Signed-off-by: flaviacapacia <154066227+flaviacapacia@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -2,11 +2,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Controllers
 builder.Services.AddControllers();
+
+// DbContext com SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
+// Endpoints
 app.MapControllers();
+
 app.Run();
+
