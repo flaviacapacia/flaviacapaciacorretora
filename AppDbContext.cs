0855b0eb702ba86df110eae42dd472a7ebfb0085
flaviacapacia <154066227+flaviacapacia@users.noreply.github.com>
2025-11-14 09:26:44 -0300

    Update AppDbContext.cs
    Signed-off-by: flaviacapacia <154066227+flaviacapacia@users.noreply.github.com>

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,6 +1,2 @@
-public class AppDbContext : DbContext
-{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+public DbSet<Imoveis> Imoveis { get; set; }
 
-    public DbSet<Imovel> Imoveis { get; set; }
-}
